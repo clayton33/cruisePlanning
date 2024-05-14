81af6a1c6fae84af91fe898fc16b2b5830e34581
Layton <layton.chantelle@gmail.com>
2024-05-14 15:38:35 -0300

    add more documentation

--- R/read.R
+++ R/read.R
@@ -1,9 +1,28 @@
 #' @title Read cruise input file
 #'
 #' @description Read in mission input file required for `runCruisePlanning`. It will
-#' check for required columns.
+#' check for required columns. See details for required columns.
 #'
-#' @param file a connection or a character string giving the name of the file to load
+#' @param file a connection or a character string giving the name of the file to load.
+#'
+#' @details
+#' @section file:
+#' At a minimum, the supplied `.csv` file should include the following variables
+#' \tabular{ll}{
+#' \strong{variable name} \tab \strong{variable description} \cr
+#' lon_dd \tab A numeric value indicating longitude in decimal degrees. \cr
+#' lat_dd \tab A numeric value indicating latitude in decimal degrees. \cr
+#' optime \tab A numeric value indicating the total expected operational time in hours at given lon_dd, lat_dd. \cr
+#' transit_speed \tab A numeric value indicating the transit speed of the vessel.
+#' }
+#' Additional variables that are suggested (but not limited to):
+#' \tabular{ll}{
+#' \strong{variable name} \tab \strong{variable description} \cr
+#' xoptime \tab A numeric value indicating any extra operational time in hours at given lon_dd, lat_dd. \cr
+#' station \tab A character string indicating the name of the station. \cr
+#' operation \tab An character string indicating the specific operation order for the given operation type (e.g. Net_CTD, Net_CTD_Argo, Net_2_CTD). \cr
+#' type \tab A character string indicating the operation type (e.g. `operation`, `transit`).\cr
+#' }
 #'
 #' @author Chantelle Layton
 #'
@@ -14,17 +33,15 @@
 
 readCruiseInput <- function(file) {
   d <- utils::read.csv(file = file)
-  # define required columns
+  # define minimum required columns
   requiredColumns <- c("lon_dd",
                        "lat_dd",
                        "optime",
-                       "xoptime",
                        "transit_speed")
   # define required columns description (for error messages)
   requiredColumnsDescrip <- c("A numeric value indicating longitude in decimal degrees.",
                               "A numeric value indicating latitude in decimal degrees.",
                               "A numeric value indicating the total expected operational time in hours at given lon_dd, lat_dd.",
-                              "A numeric value indicating any extra operational time in hours at given lon_dd, lat_dd",
                               "A numeric value indicating the transit speed of the vessel.")
   # check for required columns
   dnames <- names(d)
--- R/run.R
+++ R/run.R
@@ -2,17 +2,38 @@
 #'
 #' @description Run cruise planning. The distance to next set of coordinate, `dist_to_next`, transit time to next set of coordinates,
 #' `transit_time`, arrival time on the coordinates, `arrival_time`, departure time at the coordinates, `departure_time`,
-#' and the depth at the set of coordinates is added to the provided
+#' and the depth at the set of coordinates is added to the provided variables in the supplied `file`.
 #'
-#' @param file a connection or a character string giving the name of the file to load.
+#' @param file a connection or a character string giving the name of the `.csv` file to load. See details on the minimum
+#' and suggested variables.
 #' @param startTime a `POSIXct` format of the start date time. Suggested to use `ISOdatetime()` for simplicity.
 #' @param writeCsv a logical value, if `TRUE` a `.csv` file will be written in the working directory using the provided
 #' filename, with the current local date and time appended to the end of the filename.
 #' @param writeSubsetCsv a logical value, if `TRUE` a `.csv` file will be written in the working directory using the provided
-#' filename, with the current local date and time appended to the end of the filename using provided names in `abbreviatedNames`
-#' @param subsetNames a vector of names in provided filename to be output into a `.csv`. Ignored if `writeAbbreviatedCsv = FALSE`.
+#' filename, with the current local date and time appended to the end of the filename using provided names in `subsetNames`
+#' @param subsetNames a vector of names in provided `file` to be output into a `.csv`. Ignored if `writeSubsetCsv = FALSE`.
 #' Defaults are `c('lon_dd', 'lat_dd','station','operation','depth','dist_to_next', 'arrival_time','departure_time')`. The
 #' output file will provide variable names in order provided. During a cruise, this is what is readily printed off and circulated to staff.
+#'
+#' @details The following provides additional details for what to include in the provided `.csv` file.
+#' @section file:
+#' At a minimum, the supplied `.csv` file should include the following variables
+#' \tabular{ll}{
+#' \strong{variable name} \tab \strong{variable description} \cr
+#' lon_dd \tab A numeric value indicating longitude in decimal degrees. \cr
+#' lat_dd \tab A numeric value indicating latitude in decimal degrees. \cr
+#' optime \tab A numeric value indicating the total expected operational time in hours at given lon_dd, lat_dd. \cr
+#' transit_speed \tab A numeric value indicating the transit speed of the vessel.
+#' }
+#' Additional variables that are suggested (but not limited to):
+#' \tabular{ll}{
+#' \strong{variable name} \tab \strong{variable description} \cr
+#' xoptime \tab A numeric value indicating any extra operational time in hours at given lon_dd, lat_dd. \cr
+#' station \tab A character string indicating the name of the station. \cr
+#' operation \tab An character string indicating the specific operation order for the given operation type (e.g. Net_CTD, Net_CTD_Argo, Net_2_CTD). \cr
+#' type \tab A character string indicating the operation type (e.g. `operation`, `transit`).\cr
+#' }
+#'
 #'
 #' @author Chantelle Layton
 #'
